0b4f8cc22cacb1fa31c7cbe689115cb55b7f5179
Hisyam <hisyamskurniawan18@gmail.com>
2023-02-09 16:32:00 +0700

    feat: add update user schema validation

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -2,17 +2,43 @@ import { object, string, array, enum as zodEnum, boolean, TypeOf } from 'zod';
 
 const rolesEnum = zodEnum(['Employee', 'Admin', 'Manager']);
 
+const payload = {
+  username: string({
+    required_error: 'username is required',
+  }).min(1, 'Username cannot be empty'),
+  roles: array(rolesEnum).nonempty({ message: 'Roles cannot be empty' }),
+  active: boolean().default(true),
+};
+
 export const createUserSchema = object({
   body: object({
-    username: string({
-      required_error: 'username is required',
-    }).min(1, 'Username cannot be empty'),
+    ...payload,
     password: string({
       required_error: 'password is required',
     }).min(6, 'Passowrd must be at least 6 characters long'),
-    roles: array(rolesEnum).nonempty({ message: 'Roles cannot be empty' }),
-    active: boolean().default(true),
+  }),
+});
+
+export const updateUserSchema = object({
+  body: object({
+    ...payload,
+    id: string({
+      required_error: 'id field is required',
+    }),
+    password: string()
+      .min(6, 'Password must be at least 6 characters long')
+      .optional(),
+  }),
+});
+
+export const deleteUserSchema = object({
+  body: object({
+    id: string({
+      required_error: 'id filed is required',
+    }),
   }),
 });
 
 export type CreateUserInput = TypeOf<typeof createUserSchema>;
+export type UpdateUserInput = TypeOf<typeof updateUserSchema>;
+export type DeleteUsersInput = TypeOf<typeof deleteUserSchema>;
